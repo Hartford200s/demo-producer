9aa537e461f3478cf05c9cb4c24824010ab07efa
Tai <test>
2020-05-13 09:26:22 +0800

    springcloud(三)：服务提供与调用 (finish) http://www.ityouknow.com/springcloud/2017/05/12/eureka-provider-constomer.html

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class HelloController { 
 	
 	@RequestMapping("/hello")
     public String index(@RequestParam String name) {
