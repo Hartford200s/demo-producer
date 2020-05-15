1d854acb3e7a3f003e0d363b757be12f7391af40
Tai <test>
2020-05-15 14:18:36 +0800

    springcloud(十一)：服务网关Zuul高级篇 (finish) http://www.ityouknow.com/springcloud/2018/01/20/spring-cloud-zuul.html

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController { 
 	
+	private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+	
 	@RequestMapping("/hello")
     public String index(@RequestParam String name) {
         return "hello "+name+"，this is first messge";
+    }
+	
+	@RequestMapping("/hello2")
+    public String hello2(@RequestParam String name) {
+		logger.info("request two name is "+name);
+	    try{
+	        Thread.sleep(1000000);//模擬此method有異常
+	    }catch ( Exception e){
+	        logger.error(" hello two error",e);
+	    }
+	    return "hello "+name+"，this is two messge";
     }
 }
